df11efc560dbe70d54b87e1078d78ca9f32f758d
Kelly Austin <kaustly@gmail.com>
2016-10-24 15:45:28 -0400

    express

--- index.js
+++ index.js
@@ -2,10 +2,34 @@
 //require mongoose(the orm)
 var mongoose = require("mongoose");
 //connect to mongo
-mongoose.connect("mongod://localhost/students")
-//model (schema)
-var Student.model("Student", {
+mongoose.connect("mongodb://localhost/students")
+//define model (schema)
+var Student = mongoose.model("Student", {
   name: String
-})
+});
 //seed data
-new Student({name:"Teddy"});
+var s1 = new Student({name:"Teddy"});
+s1.save();
+var s2 = new Student({name:"Britton"});
+s2.save(function(){
+  console.log("at least s2 saved")
+});
+//require express / app
+var express = require("express");
+var app = express();
+// listen on local host
+app.listen(3000, function(){
+  console.log("http://localhost:3000/")
+});
+//a single route
+app.get("/", function(req,res){
+  res.send('hello')
+});
+
+
+
+
+//Student.find({}, function(err, students){
+  //console.log("Students:");
+  //console.log(students);
+//})
